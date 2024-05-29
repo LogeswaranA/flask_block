918b9824b7893cd560014a18172792bcbc707026
Logeswaran A <lokeshwaran.a82@gmail.com>
2024-05-29 15:26:35 +0400

    added connection between the nodes

--- Main.py
+++ Main.py
@@ -14,3 +14,6 @@ if __name__ == "__main__":
    port = int(sys.argv[2])
    node = Node(ip,port)
    node.startP2P()
+
+   if port == 10002:
+      node.p2p.connect_with_node('localhost',10001)
--- SocketCommunication.py
+++ SocketCommunication.py
@@ -7,4 +7,18 @@ class SocketCommunication(Node):
 
     def startSocketCommunication(self):
         self.start()
+    
+    def inbound_node_connected(self, node):
+        print("inbound connection")
+        self.send_to_node(node,"I am the node, you connected to")
+        return super().inbound_node_connected(node)
+    
+    def outbound_node_connected(self, node):
+        print("outbound connection")
+        self.send_to_node(node,"I am the node, who initialized the connection")
+        return super().outbound_node_connected(node)
+    
+    def node_message(self, node, data):
+        print("message:::",data)
+        return super().node_message(node, data)
 
